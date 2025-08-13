32c6eaabb1066836501b9e8417ee9c053e5131aa
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2025-08-12 22:37:41 -1100

    Update script.js
    Rock ,Paper and Scissors played   only in the consloe

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 function computerPlay() {
-    let computerRandom =;
+  let computerRandom = Math.floor(Math.random() * 3);
+  switch (computerRandom) {
+    case 0:
+      return "Rock";
 
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection == "Rock" && computerSelction == "Scissors") ||
+    (playerSelection == "Scissors" && computerSelction == "Paper") ||
+    (playerSelection == "Paper" && computerSelction == "Rock")
+  ) {
+    return "you win !";
+  } else if (
+    (computerSelction == "Rock" && playerSelection == "Scissors") ||
+    (computerSelction == "Scissors" && playerSelection == "Paper") ||
+    (computerSelction == "Rock" && playerSelection == "Scissors")
+  ) {
+    return "you lose !";
+  } else {
+    return "equals ";
+  }
+}
+alert("welcometo rock paper scissors game !");
+const playerSelection = prompt("choose : Rock , Paper or Scissors ");
+const computerSelction = computerPlay();
+
+console.log(computerSelction);
+console.log(playRound(playerSelection, computerSelction));
+
+let score;
+let attempts;
+function game() {
+  for (let i = 0; i < 5; i++) {
+    attempts++;
+    console.log(playRound(playerSelection, computerSelction));
+    alert("sorry you reached the limit !");
+  }
 }
